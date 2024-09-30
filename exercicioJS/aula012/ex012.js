bfa92e8ae6e440eda22c8678dbe947e4187dec36
Wellington Ferreira <welferreiratom@gmail.com>
2024-09-30 20:52:56 -0300

    testando

--- exercicioJS/aula012/ex012.js
+++ exercicioJS/aula012/ex012.js
@@ -1,8 +1,10 @@
-var idade = 65
-if(idade < 16){
-    console.log('Não vota')
-} else if(idade < 18 && idade >=65){
-    console.log('Voto opcional')
-}else{
-    console.log('Voto obrigatorio')
+var agora = new Date()
+var hora = agora.getHours
+console.log(`Agora são exatamanete ${agora  } horas`)
+if(hora < 12){
+    console.log(`Bom dia`)
+}else if(hora <=18){
+    console.log(`Boa tarde`)
+}else {
+    console.log(`Boa noite`)
 }
