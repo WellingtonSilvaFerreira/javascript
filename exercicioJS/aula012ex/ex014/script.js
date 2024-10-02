ac460121bc90cf73f0828175d550c5a6d1ca9cc4
Wellington Ferreira <welferreiratom@gmail.com>
2024-10-01 21:15:26 -0300

    atualizando

--- exercicioJS/aula012ex/ex014/script.js
+++ exercicioJS/aula012ex/ex014/script.js
@@ -1,1 +1,23 @@
- 
+ function carregar(){
+
+ var msg = document.getElementById('msg')
+ var img = document.querySelector('#imagem')
+ var data = new Date()
+ var hora = data.getHours()
+ //var hora = 10
+ msg.innerHTML = `Agora são ${hora}:00 horas.`
+
+ if(hora >= 0 && hora < 12){
+    //Bom dia
+    img.src = 'manha.jpg'
+    document.body.style.background= '#FAB074'
+ }else if(hora >= 12 && hora < 18){
+    //Boa tarde
+    img.src='tarde.jpg'
+    document.body.style.background= '#DC8935'
+ }else{
+    //Boa noite
+    img.src = 'noite.jpg'
+    document.body.style.background = '#071C1D'
+ }
+ }
