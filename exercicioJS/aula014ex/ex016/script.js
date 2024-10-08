4558db4032fb00f2059a11750f9a74d3f20e86f6
Wellington Ferreira <welferreiratom@gmail.com>
2024-10-07 21:45:28 -0300

    inicio de nova aula

--- exercicioJS/aula014ex/ex016/script.js
+++ exercicioJS/aula014ex/ex016/script.js
@@ -1,1 +1,27 @@
- 
+ function contar(){
+    let ini = document.querySelector('#ini')
+    let fim = document.querySelector('#fim')
+    let passo = document.querySelector('#passo')
+    let res = document.querySelector('#res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados')
+    }else{
+        res.innerHTML='Contando'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(i < f){
+            for( let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1f449} `
+            }
+        } else {
+            for(let c = i; c >= f; c-= p){
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+            res.innerHTML += '\u{1f3c1}'
+        
+        }
+        
+    }
