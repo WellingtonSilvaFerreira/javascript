fea1bad57f3dd6f4b2b8caf502330748d662a70b
Wellington Ferreira <welferreiratom@gmail.com>
2024-10-09 19:44:01 -0300

    inicio

--- exercicioJS/aula014ex/ex016/script.js
+++ exercicioJS/aula014ex/ex016/script.js
@@ -5,12 +5,18 @@
     let res = document.querySelector('#res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML='Impossivel contar'
         window.alert('[ERRO] Faltam dados')
+        
     }else{
-        res.innerHTML='Contando'
+        res.innerHTML='Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if( p<=0){
+            alert('Passo inválido! Considerando passo 1')
+            p=1
+        }
         if(i < f){
             for( let c = i; c <= f; c += p){
             res.innerHTML += `${c} \u{1f449} `
